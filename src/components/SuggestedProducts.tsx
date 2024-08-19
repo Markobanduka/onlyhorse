d7a516d2cc7bd30ac6f8502e0dc3ade237020280
Markobanduka <banduka44@gmail.com>
2024-08-19 20:33:07 +0200

    added suggested product and merch page

---
+++ src/components/SuggestedProducts.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SuggestedProduct from "./SuggestedProduct";
+import { products } from "@/dummy_data";
+
+const SuggestedProducts = () => {
+  return (
+    <div
+      className="lg:w-2/5 hidden lg:flex flex-col gap-3 px-2 sticky top-0 right-0
+    h-screen ml-3"
+    >
+      <div className="flex flex-col gap-2 mt-20">
+        <p className="uppercase text-muted-foreground">Recommended Products</p>
+        <div className="grid grid-cols-2 gap-4">
+          {products.map((product) => (
+            <SuggestedProduct key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SuggestedProducts;
--- src/components/home/home-screen/Posts.tsx
+++ src/components/home/home-screen/Posts.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import Post from "./Post";
 import UnderlinedText from "@/components/decorators/UnderlinedText";
+import PostSkeleton from "@/components/skeletons/PostSkeloton";
+import { admin, posts, user } from "@/dummy_data";
 
 const Posts = () => {
-  const posts = [];
+  const isLoading = false;
+
   return (
     <div>
-      {posts.length === 0 && (
+      {!isLoading &&
+        posts.map((post) => (
+          <Post
+            key={post.id}
+            post={post}
+            admin={admin}
+            isSubscribed={user.isSubscribed}
+          />
+        ))}
+
+      {isLoading && (
+        <div className="mt-10 px-3 flex flex-col gap-10">
+          {[...Array(3)].map((_, i) => (
+            <PostSkeleton key={i} />
+          ))}
+        </div>
+      )}
+
+      {!isLoading && posts.length === 0 && (
         <div className="mt-10 px-3">
           <div className="flex flex-col items-center space-y-3 w-full md:w-3/4 mx-auto">
             <p className="text-xl font-semibold">
